c8c03f658c48869188d72cdd6274a658756db61e
Tony Dattolo <anthonydattolo@gmail.com>
2022-07-29 16:05:57 -0400

    added error handling popup for invalid messages

--- frontend/components/Alerts/Alert.tsx
+++ frontend/components/Alerts/Alert.tsx
@@ -1,7 +1,9 @@
+import { FiX } from "react-icons/fi";
+
 type AlertProps = {
   message: string;
   type?: "success" | "error" | "info";
-  onClose?: () => void;
+  onClick?: () => void;
 };
 
 const Alert: React.FC<AlertProps> = ({
@@ -21,21 +23,20 @@ const Alert: React.FC<AlertProps> = ({
   return (
     <>
       <div
-        className={`bg-${color}-100 border border-${color}-400 text-${color}-700 px-4 py-3 rounded relative`}
+        // className={`bg-${color}-100 border border-${color}-400 text-${color}-700 px-4 py-3 rounded`}
+        className={`bg-cyan-100 border border-cyan-400 text-cyan-700 px-4 py-3 rounded transition duration-300 ease-out`}
         role="alert"
       >
-        <strong className="font-bold">message error</strong>
-        <span className="block sm:inline">{message}</span>
-        <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-          <svg
-            className={`fill-current h-6 w-6 text-${color}-500`}
-            role="button"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <title>Close</title>
-            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
-          </svg>
+        <strong className="font-bold">message error &nbsp;&nbsp;</strong>
+        <span className="">
+          {message}
+          <FiX
+            size="24"
+            className="inline ml-2 text-red-500 hover:text-red-500 cursor-pointer bg-transparent"
+            onClick={() => {
+              console.log("clicked");
+            }}
+          />
         </span>
       </div>
     </>
--- frontend/components/BottomTextInput/BottomTextInput.tsx
+++ frontend/components/BottomTextInput/BottomTextInput.tsx
@@ -1,6 +1,6 @@
 import { AiFillPlusCircle } from "react-icons/ai";
 import supabase from "@/utils/supabase";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import Message from "@/types/Message";
 import validateMessage from "@/utils/validateMessage";
@@ -15,6 +15,7 @@ const BottomTextInput: React.FC = () => {
     // check if message is empty
     if (validateMessage(message) === false) {
       console.log("invalid message");
+      // set invalid message to true for 5 seconds
       setInvalidMessage(true);
       return;
     }
@@ -32,45 +33,60 @@ const BottomTextInput: React.FC = () => {
     }
   };
 
+  useEffect(() => {
+    if (invalidMessage === true) {
+      setTimeout(() => {
+        setInvalidMessage(false);
+      }, 5000);
+    }
+  }, [invalidMessage]);
+
   return (
     <>
-      <div className="flex flex-col mb-25">
-        {invalidMessage && (
-          <div>
-            <Alert
-              message="Invalid message"
-              type="error"
-              onClose={() => setInvalidMessage(false)}
-            />
-          </div>
-        )}
+      {/* <div className="flex flex-col"> */}
+      {invalidMessage && (
+        <div className="flex fixed left-2 right-2 bottom-16 h-12 transition duration-300 ease-in-out">
+          <Alert
+            message="Invalid message"
+            onClick={() => setInvalidMessage(false)}
+          />
+        </div>
+      )}
 
-        <div
-          className="flex flex-row items-center justify-between 
+      <div
+        className="flex flex-row items-center justify-between 
             fixed left-2 right-2 bottom-2 h-12 
             rounded-lg shadow-lg 
             bg-gray-400 dark:bg-gray-600
+            overflow-y-auto
+
           "
-        >
-          <AiFillPlusCircle
-            size="30"
-            className="text-green-500 shadow-lg dark:shadow-lg mx-2 dark:text-green-500 hover:text-green-500 cursor-pointer bg-transparent"
-            onClick={handleSendMessage}
-          />
-          <input
-            type="text"
-            placeholder="Enter a message..."
-            className="bottom-bar-input"
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                handleSendMessage();
-              }
-            }}
-          />
-        </div>
+        // contentEditable={true}
+      >
+        <AiFillPlusCircle
+          size="30"
+          className="text-green-500 shadow-lg dark:shadow-lg mx-2 dark:text-green-500 hover:text-green-500 cursor-pointer bg-transparent"
+          onClick={handleSendMessage}
+        />
+        <input
+          type="text"
+          placeholder="Enter a message..."
+          className="font-semibold w-full max-w-full
+          bg-transparent outline-none 
+          ml-0 mr-auto
+          text-gray-500  dark:text-gray-400 placeholder-gray-500
+          cursor-text"
+          value={message}
+          onChange={(event) => setMessage(event.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              handleSendMessage();
+            }
+          }}
+          contentEditable={true}
+        />
       </div>
+      {/* </div> */}
     </>
   );
 };
